b3698780e6ecbcb6004fd708e077fee8b5bb02c3
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-05-29 00:28:37 -0400

    sbatch

--- sbatch_manual.sh
+++ sbatch_manual.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=ashton
 #SBACTH --qos=ashton
 #SBATCH --job-name=mongarud-manual
-#SBATCH --output=$manual/log_%j.log      # Standard output and error log
+#SBATCH --output=manual/log_%j.log      # Standard output and error log
 #SBATCH --time=12:00:00
 #SBATCH --mem=4G
 #SBATCH --gres=gpu:1
@@ -10,4 +10,4 @@
 mkdir -p manual
 
 conda activate gqeimplementation
-python3 main.py --do_train --do_valid --do_test --data_path "FB15k-237" --num_epochs 2 --batch_size 64 --test_batch_size 32 --learning_rate 0.0001
+python3 main.py --do_train --do_valid --do_test --data_path "FB15k-237" --num_epochs 40 --batch_size 350 --test_batch_size 150 --learning_rate 0.0001
