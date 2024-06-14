72ad7386a66f45126bfea38d60ffc664bb9fd647
Rudy <rudraksh.monga@mail.utoronto.ca>
2024-06-14 01:31:26 -0400

    NELL training

--- sbatch_manual.sh
+++ sbatch_manual.sh
@@ -4,10 +4,10 @@
 #SBATCH --job-name=mongarud-manual
 #SBATCH --output=manual/log_%j.log      # Standard output and error log
 #SBATCH --time=12:00:00
-#SBATCH --mem=4G
+#SBATCH --mem=20G
 #SBATCH --gres=gpu:1
 
 mkdir -p manual
 
 conda activate gqeimplementation
-python3 main.py --do_train --do_valid --do_test --data_path "NELL-betae" --num_epochs 40 --batch_size 200 --test_batch_size 75 --learning_rate 0.0001
+python3 main.py --do_train --do_valid --do_test --data_path "NELL-betae" --num_epochs 40 --batch_size 350 --test_batch_size 150 --learning_rate 0.0001
